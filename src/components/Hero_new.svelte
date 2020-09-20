99e8dadb911fc5bac16cc8d0a5be0291926d9ef6
Naveen <catchspider2002@gmail.com>
2020-09-20 23:49:46 +0530

    first commit

---
+++ src/components/Hero_new.svelte
@@ -0,0 +1,93 @@
+<script>
+  export let title, tags;
+  let newtags = [];
+  tags.forEach(letter => {
+    newtags.push({ tag: letter });
+  });
+  console.log(title);
+  let lang = tags[0];
+  let color;
+
+  switch (lang) {
+    case "JAVASCRIPT":
+      color = "53.4,93.1%,54.3%";
+      break;
+    case "SVELTE":
+      color = "14.6,100%,50%";
+      break;
+    case "CSS":
+      color = "228,78%,52%";
+      break;
+    case "TAILWIND CSS":
+      color = "177,52%,46%";
+      break;
+    case "SVG":
+      color = "36,100%,62%";
+      break;
+    case "HTML":
+      color = "13,77%,52%";
+      break;
+    case "REACT":
+      color = "192.9,95.1%,68.2%";
+      break;
+  }
+
+  import html2canvas from "html2canvas";
+  import JavascriptSvg from "./JavascriptSVG.svelte";
+
+  function screenshot() {
+    console.log("screenshot");
+    // html2canvas(document.getElementById("target1")).then(function(canvas) {
+    html2canvas(document.body, {
+      width: 1200,
+      height: 627,
+      y: 50
+    }).then(function (canvas) {
+      console.log("before append");
+      document.body.appendChild(canvas);
+    });
+  }
+</script>
+
+<style>
+  :root {
+    --bg: black;
+    --fg: white;
+    /* 		--bg: white;
+		--fg: black; */
+  }
+  :global(body) {
+    font-size: 20px;
+  }
+  @media (max-width: 1440px) {
+    :global(body) {
+      font-size: 18px;
+    }
+  }
+  @media (max-width: 768px) {
+    :global(body) {
+      font-size: 16px;
+    }
+  }
+  @media (max-width: 400px) {
+    :global(body) {
+      font-size: 14px;
+    }
+  }
+  .header {
+    /* min-height: 630px; */
+    /* padding: 2em; */
+    display: grid;
+    grid-template-columns: 1fr;
+    place-content: center;
+    place-items: center;
+    background-color: hsl(var(--theme-color));
+  }
+</style>
+
+-
+<section class="header" style="background-color: hsl({color});">
+  <JavascriptSvg {color} {lang} />
+</section>
+
+<button on:click={screenshot}>to image</button>
---
+++ src/routes/[slug].svelte
@@ -0,0 +1,63 @@
+<script context="module">
+  export async function preload({ params, query }) {
+    // the `slug` parameter is available because
+    // this file is called [slug].svelte
+    const res = await this.fetch(`${params.slug}.json`);
+    const data = await res.json();
+
+    if (res.status === 200) {
+      return { post: data };
+    } else {
+      this.error(res.status, data.message);
+    }
+  }
+</script>
+
+<script>
+  export let post;
+  import NavBar from "../components/NavBar.svelte";
+  import Blog from "../components/Blog.svelte";
+</script>
+
+<style>
+  .content :global(h2) {
+    font-size: 1.4em;
+    font-weight: 500;
+  }
+
+  .content :global(ul) {
+    line-height: 1.5;
+  }
+
+  .content :global(li) {
+    margin: 0 0 0.5em 0;
+  }
+</style>
+
+<svelte:head>
+  <title>{post.title}</title>
+  <link rel="canonical" href="https://blog.periodic-table.io/{post.slug}" />
+  <meta name="description" content={post.description} />
+  <meta name="keywords" content={post.keywords} />
+
+  <!-- Open Graph / Facebook -->
+  <meta property="og:type" content="blog" />
+  <meta property="og:url" content="https://blog.periodic-table.io/{post.slug}" />
+  <meta property="og:title" content={post.title} />
+  <meta property="og:description" content={post.description} />
+  <meta property="og:image" content="https://blog.periodic-table.io/social/{post.slug}.png" />
+
+  <!-- Twitter -->
+  <meta property="twitter:card" content="summary_large_image" />
+  <meta property="twitter:url" content="https://blog.periodic-table.io/{post.slug}" />
+  <meta property="twitter:title" content={post.title} />
+  <meta property="twitter:description" content={post.description} />
+  <meta property="twitter:image" content="https://blog.periodic-table.io/social/{post.slug}.png" />
+</svelte:head>
+<NavBar />
+
+<Blog title={post.title} tags={post.tags} />
+
+<div class="content">
+  {@html post.html}
+</div>
